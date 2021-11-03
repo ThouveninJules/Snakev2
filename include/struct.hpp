145d3f7220aaef5c53b1c6c5c5b6f346b159b62c
Jules Thouvenin <81535460+ThouveninJules@users.noreply.github.com>
2021-11-03 08:34:24 +0100

    code de base

---
+++ include/struct.hpp
@@ -0,0 +1,9 @@
+#ifndef "STRUCT_HPP_"
+#define "STRUCT_HPP_"
+
+struct snake{
+  int x;
+  int y;
+};
+
+#endif /*"STRUCT_HPP"*/
---
+++ source/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/struct.hpp"
+#include <sfml/graphics.hpp>
+
+
+int main(){
+
+  snake snake[100];
+
+  return 0;
+};
